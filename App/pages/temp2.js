98afbb679147230c04b42ef5385604197bae6ac9
lunatix01 <lunatix1@protonmail.com>
2021-08-29 13:21:47 -0700

    fixing responsive nav to products page

---
+++ App/pages/temp2.js
@@ -0,0 +1,15 @@
+import Navbar2 from "../components/Navbar2"
+const temp2 = () => {
+    return (
+        <>
+            <div>
+                <Navbar2/>
+            </div>
+            <div>
+                
+            </div>
+        </>
+    )
+}
+
+export default temp2
