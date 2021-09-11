e4a28389bc6d181195279216a083371ae04bfe03
lunatix01 <lunatix1@protonmail.com>
2021-09-11 12:20:14 -0700

    fixes add product

--- App/pages/temp2.js
+++ App/pages/temp2.js
@@ -1,12 +1,40 @@
 import Navbar2 from "../components/Navbar2"
+import CreateProducts from "../components/CreateProducts"
+import React , { useEffect , useRef , useState } from "react"
+
+export const modalContext = React.createContext()
+
 const temp2 = () => {
+    let [ isOpen , setIsOpen ] = useState(false)
+    const ref = useRef(null)
+    useEffect( () => {
+        if(ref){
+        let handler = (event) => {
+            if(!ref.current || !ref.current.contains(event.target)) {
+                setIsOpen(false)
+            }
+        }
+        document.addEventListener("mousedown" , handler )
+        
+        return  () =>{
+            document.removeEventListener("mousedown", handler )
+        }
+    }},[ref])
+
     return (
         <>
             <div>
                 <Navbar2/>
             </div>
-            <div>
-                
+            <div className="py-12 px-32">
+                <div className="flex justify-center md:justify-start">
+                    <button onClick={ ()=> setIsOpen(!isOpen) } className="bg-indigo-600 rounded-lg h-10 w-32  hover:animate__animated hover:animate__pulse text-white md:text-lg font-semibold hover:bg-indigo-500 hover:transition hover:ease-in-out">
+                        Add product
+                    </button>
+                </div>
+                <modalContext.Provider value={ { modalState : [ isOpen,setIsOpen ] } }>
+                { isOpen && <div ref={ref}> <CreateProducts/> </div>}
+                </modalContext.Provider>
             </div>
         </>
     )
--- App/tailwind.config.js
+++ App/tailwind.config.js
@@ -48,6 +48,18 @@ module.exports = {
     },
   },
   plugins: [
-    require('tailwind-scrollbar')
+    require('tailwind-scrollbar'),
+    require('tailwindcss-animatecss')({
+      classes: ['animate__animated', 'animate__fadeIn', 'animate__bounceIn', 'animate__lightSpeedOut','animate__backOutUp','animate__pulse'],
+      settings: {
+        animatedSpeed: 1000,
+        heartBeatSpeed: 1000,
+        hingeSpeed: 2000,
+        bounceInSpeed: 750,
+        bounceOutSpeed: 750,
+        animationDelaySpeed: 1000
+      },
+      variants: ['responsive', 'hover', 'reduced-motion'],
+    }),
   ],
 }
